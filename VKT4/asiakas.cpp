b12d50ccfb551f5cb79e94d075e248b664a0efed
JHPirinen <joel.pirinen@gmail.com>
2025-01-31 09:13:25 +0200

    Finished VKT4

--- VKT4/asiakas.cpp
+++ VKT4/asiakas.cpp
@@ -1,9 +1,8 @@
 #include "asiakas.h"
 
-Asiakas::Asiakas(string asiakasNimi, double henkRaja){
-    nimi = asiakasNimi;
-    Luottotili(nimi, henkRaja);
-    cout << "Asiakkuus luotu" << nimi << ":lle" << endl;
+Asiakas::Asiakas(string omistajaNimi, double henkRaja) : kayttotili(omistajaNimi), luottotili(omistajaNimi, henkRaja){
+    nimi = omistajaNimi;
+    cout << "Asiakkuus luotu " << nimi << ":lle" << endl;
 }
 
 string Asiakas::getNimi()
@@ -13,37 +12,62 @@ string Asiakas::getNimi()
 
 void Asiakas::showSaldo()
 {
-    cout << "Kayttotilin " << nimi << " saldo on" << kayttotili.getBalance() << endl;
-    cout << "luottotilin " << nimi << " saldo on" << luottotili.getBalance() << endl;
+    cout << "Kayttotilin " << nimi << ": saldo on " << kayttotili.getBalance() << endl;
+    cout << "luottotilin " << nimi << ": saldo on " << luottotili.getBalance() << endl;
 }
 
 bool Asiakas::talletus(double sum)
 {
-    kayttotili.deposit(sum);
-    cout << "Kaytotili" << nimi << ": talletus " << sum << " tehty, tilin saldo " << kayttotili.getBalance() << endl;
+    if(kayttotili.deposit(sum) == true){
+        cout << "Kayttotili " << nimi << ": talletus " << sum << " tehty, tilin saldo " << kayttotili.getBalance() << endl;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool Asiakas::nosto(double sum)
 {
-    kayttotili.withdraw(sum);
-    cout << "Kaytotili" << nimi << ": nosto " << sum << " tehty, tilin saldo " << kayttotili.getBalance() << endl;
+    if(kayttotili.withdraw(sum) == true){
+        cout << "Kayttotili " << nimi << ": nosto " << sum << " tehty, tilin saldo " << kayttotili.getBalance() << endl;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool Asiakas::luotonMaksu(double sum)
 {
-    luottotili.deposit(sum);
-    cout << "Luottotili" << nimi << ": masku " << sum << " tehty, luottoa jaljella " << luottotili.getBalance() << endl;
+    if(luottotili.deposit(sum) == true){
+        cout << "Luottotili " << nimi << ": masku " << sum << " tehty, luottoa jaljella " << luottotili.getBalance() << endl;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool Asiakas::luotonNosto(double sum)
 {
-    luottotili.withdraw(sum);
-    cout << "Luottotili" << nimi << ": nosto " << sum << " tehty, luottoa jaljella " << luottotili.getBalance() << endl;
+    if(luottotili.withdraw(sum) == true){
+        cout << "Luottotili " << nimi << ": nosto " << sum << " tehty, luottoa jaljella " << luottotili.getBalance() << endl;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool Asiakas::tiliSiirto(double sum, Asiakas & saajaNimi)
 {
-    cout << "Kaytotili: " << nimi << " siirtaa " << sum << " " << saajaNimi.nimi << ":lle" << endl;
-    nosto(sum);
-    saajaNimi.talletus(sum);
+    cout << "Kayttotili " << nimi << ": siirtaa " << sum << " " << saajaNimi.nimi << ":lle" << endl;
+    if(nosto(sum) == true){
+        saajaNimi.talletus(sum);
+        return true;
+    }
+    else{
+        return false;
+    }
 }
--- VKT4/main.cpp
+++ VKT4/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include "asiakas.h"
-#include "luottotili.h"
-#include "pankkitili.h"
 
 using namespace std;
 
 int main()
 {
     Asiakas num1("Jari", 2000);
+    num1.talletus(300);
     num1.showSaldo();
-    num1.talletus(250);
-    num1.luotonNosto(150);
     cout << endl;
     Asiakas num2("Pekka", 3000);
+    num2.luotonNosto(500);
     num2.showSaldo();
-    num1.tiliSiirto(50, num2.getNimi());
+    cout << endl;
+    num1.tiliSiirto(100, num2);
+    num1.showSaldo();
+    num2.showSaldo();
+    cout << endl;
+    num2.nosto(100);
+    num2.luotonMaksu(100);
+    num1.showSaldo();
+    num2.showSaldo();
     return 0;
 }
